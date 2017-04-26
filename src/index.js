acaafb4d1e5d36079a47be3eadad8c38fc84a69c
Horace Nelson <horaceshmorace@gmail.com>
2017-04-26 16:39:17 -0400

    Change to use a named function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-export const handler = function (event, context) {
+export function handler (event, context) {
   const error = null
   context.done(error, { message: `This is a Lamba response! You sent the following in the myParam parameter: ${event.myParam}.` })
 }
